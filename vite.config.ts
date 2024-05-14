d645d95f0d3dc113a0a50084745232ec560815b7
delalegion <nielotweb@gmail.com>
2024-05-14 18:15:40 +0200

    Modify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
-export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
-	server: {
-		host: true
-	}
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		// vite config
+		plugins: [enhancedImages(), sveltekit()],
+		server: {
+			host: true
+		},
+		define: {
+		  __APP_ENV__: JSON.stringify(env.APP_ENV),
+		},
+	 }
 });
