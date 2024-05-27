6d64782dbb6fe4c7fab5de3add19a69759511b52
delalegion <nielotweb@gmail.com>
2024-05-27 14:33:30 +0200

    Change text

--- src/components/parts/about.svelte
+++ src/components/parts/about.svelte
@@ -84,7 +84,7 @@
 						<h3 class="text-dark-50 font-semibold text-h4 lg:text-h3" id="about-title">O nas</h3>
 					</div>
 					<div class="flex flex-col gap-6">
-						<h4 class="font-semibold text-p1 lg:text-h4 text-dark-50" id="about-p-1">Firma EuPol powstała 1 stycznia lutego 2006 roku w Gorzycach. Od samego początku celem nadrzędnym firmy było zadowolenie naszych Klientów. Dlatego oferujemy niezawodne , zawsze skuteczne rozwiązania w zakresie transportu osób i towarów. Zakresem  działania obejmujemy realizację zleceń transportowych flotą własną jak i naszych podwykonawców na terenie kraju i Europy. Posiadamy licencję na wykonywanie transportu krajowego i międzynarodowego w przewozie osób i towarów do 3.5 DMC.</h4>
+						<h4 class="font-semibold text-p1 lg:text-h4 text-dark-50" id="about-p-1">Firma EuPol powstała 1 stycznia 2006 roku w Gorzycach. Od samego początku celem nadrzędnym firmy było zadowolenie naszych Klientów. Dlatego oferujemy niezawodne , zawsze skuteczne rozwiązania w zakresie transportu osób i towarów. Zakresem  działania obejmujemy realizację zleceń transportowych flotą własną jak i naszych podwykonawców na terenie kraju i Europy. Posiadamy licencję na wykonywanie transportu krajowego i międzynarodowego w przewozie osób i towarów do 3.5 DMC.</h4>
 						<h5 class="text-dark-150 text-h5 font-semibold uppercase" id="about-p-2">Zatrudniamy wykwalifikowaną kadrę kierowców , dzięki czemu zapewniamy profesjonalną obsługę naszych Klientów.</h5>
 						<p class="text-dark-50 text-p2 lg:text-p1 font-semibold" id="about-p-3">W strategię rozwoju EuPol wpisane jest ciągłe doskonalenie i regularna wymiana floty oraz dopasowanie jej do potrzeb naszych Klientów.</p>
 					</div>
