0069d997a79226d10a669fade3e2a7ca3da839bd
delalegion <nielotweb@gmail.com>
2024-07-11 22:18:23 +0200

    Fix lightbox

--- src/components/buttons/primary.svelte
+++ src/components/buttons/primary.svelte
@@ -12,7 +12,7 @@
 <input type="submit" class="text-dark-950 bg-primary text-center hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
 {size === "large" ? 'px-8 py-5' : (size === "medium" ? 'px-7 py-4' : 'px-5 py-3' )}" value="Send message">
 {:else}
-<a href="{link}" type={oftype} class="text-dark-950 bg-primary text-center hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
+<a href="{link}" type={oftype} class="text-dark-950 bg-primary text-center hover:text-dark-50 hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
 {size === "large" ? 'px-8 py-5' : (size === "medium" ? 'px-7 py-4' : 'px-5 py-3' )}"><slot /></a>
 {/if}
 
@@ -22,7 +22,7 @@
 <input type="submit" class="text-dark-950 bg-primary text-center hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
 {size === "large" ? 'px-8 py-5' : (size === "medium" ? 'px-7 py-4' : 'px-5 py-3' )}" value="Wyślij wiadomość">
 {:else}
-<a href="{link}" type={oftype} class="text-dark-950 bg-primary text-center hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
+<a href="{link}" type={oftype} class="text-dark-950 bg-primary text-center hover:text-dark-50 hover:translate-y-0.5 duration-200 ease-primary active:scale-95 focus:outline-none font-semibold text-button rounded-full text-base
 {size === "large" ? 'px-8 py-5' : (size === "medium" ? 'px-7 py-4' : 'px-5 py-3' )}"><slot /></a>
 {/if}
 
